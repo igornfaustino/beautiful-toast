0bb5fd3ef0d1276f238366a1600a936077f03215
igornfaustino <igor@nfaustino.com>
2022-06-05 22:06:44 -0300

    style: make content component beautiful

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import styled from 'styled-components'
 import { useToast } from '../hooks/useToast'
+
+const MainView = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  gap: 15px;
+`
+
+const Button = styled.button`
+  background-color: #405cf5;
+  border-radius: 6px;
+  border-width: 0;
+  box-shadow: rgba(50, 50, 93, .1) 0 0 0 1px inset,rgba(50, 50, 93, .1) 0 2px 5px 0,rgba(0, 0, 0, .07) 0 1px 1px 0;
+  box-sizing: border-box;
+  color: #fff;
+  cursor: pointer;
+  font-size: 100%;
+  height: 44px;
+  padding: 0 25px;
+`
 
 const Content = () => {
   const { dispatchToast } = useToast()
 
   return (
-    <div>
-      <button onClick={() => dispatchToast({ title: 'Title', description: 'loren ipsum' })}>Add larger toast</button>
-      <button onClick={() => dispatchToast({ title: 'Title' })}>Add small toast</button>
-    </div>
+    <MainView>
+      <Button onClick={() => dispatchToast({ title: 'Title', description: 'loren ipsum' })}>Add larger toast</Button>
+      <Button onClick={() => dispatchToast({ title: 'Title' })}>Add small toast</Button>
+    </MainView>
   )
 }
 
